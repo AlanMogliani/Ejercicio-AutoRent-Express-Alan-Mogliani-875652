080de9cfd25e037a1596f795ef6acff5fbe2830a
alan mogliani <alanbrianmogliani@hotmail.com>
2025-09-10 10:18:27 -0300

    Push de Registro de Reservas

---
+++ Ejercicio AutoRent Express Alan Mogliani 875652/Registro Reserva.Designer.cs
@@ -0,0 +1,514 @@
+﻿namespace Ejercicio_AutoRent_Express_Alan_Mogliani_875652
+{
+    partial class RegistroReserva
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            label19 = new Label();
+            label20 = new Label();
+            label21 = new Label();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label22 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            comboBox4 = new ComboBox();
+            textBox7 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(13, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(241, 34);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(241, 63);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Telefono:";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(13, 91);
+            label5.Name = "label5";
+            label5.Size = new Size(30, 15);
+            label5.TabIndex = 4;
+            label5.Text = "DNI:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(13, 120);
+            label6.Name = "label6";
+            label6.Size = new Size(106, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Fecha Nacimiento:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(116, 15);
+            label7.TabIndex = 6;
+            label7.Text = "DATOS PERSONALES";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(13, 195);
+            label8.Name = "label8";
+            label8.Size = new Size(100, 15);
+            label8.TabIndex = 7;
+            label8.Text = "Fecha de Entrega:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(13, 166);
+            label9.Name = "label9";
+            label9.Size = new Size(109, 15);
+            label9.TabIndex = 8;
+            label9.Text = "DATOS DE RESERVA";
+            label9.Click += label9_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(13, 221);
+            label10.Name = "label10";
+            label10.Size = new Size(120, 15);
+            label10.TabIndex = 9;
+            label10.Text = "Fecha de Devolucion:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(377, 192);
+            label11.Name = "label11";
+            label11.Size = new Size(54, 15);
+            label11.TabIndex = 10;
+            label11.Text = "Sucursal:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(13, 144);
+            label12.Name = "label12";
+            label12.Size = new Size(672, 15);
+            label12.TabIndex = 11;
+            label12.Text = "-------------------------------------------------------------------------------------------------------------------------------------";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(12, 251);
+            label13.Name = "label13";
+            label13.Size = new Size(98, 15);
+            label13.TabIndex = 12;
+            label13.Text = "Tipo de Vehiculo:";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(12, 287);
+            label14.Name = "label14";
+            label14.Size = new Size(96, 15);
+            label14.TabIndex = 13;
+            label14.Text = "Sillas para Niños:";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(217, 289);
+            label15.Name = "label15";
+            label15.Size = new Size(58, 15);
+            label15.TabIndex = 14;
+            label15.Text = "Cantidad:";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(376, 224);
+            label16.Name = "label16";
+            label16.Size = new Size(90, 15);
+            label16.TabIndex = 15;
+            label16.Text = "Forma de Pago:";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(376, 254);
+            label17.Name = "label17";
+            label17.Size = new Size(90, 15);
+            label17.TabIndex = 16;
+            label17.Text = "Tipo de Cliente:";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(12, 325);
+            label18.Name = "label18";
+            label18.Size = new Size(0, 15);
+            label18.TabIndex = 17;
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Location = new Point(13, 340);
+            label19.Name = "label19";
+            label19.Size = new Size(146, 15);
+            label19.TabIndex = 18;
+            label19.Text = "GENERACION DE RESERVA";
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Location = new Point(13, 364);
+            label20.Name = "label20";
+            label20.Size = new Size(108, 15);
+            label20.TabIndex = 19;
+            label20.Text = "Codigo de Reserva:";
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.BackColor = SystemColors.MenuHighlight;
+            label21.Location = new Point(329, 365);
+            label21.Name = "label21";
+            label21.Size = new Size(345, 15);
+            label21.TabIndex = 20;
+            label21.Text = "ATENCION: La reserva se generara cuando se confirme la misma";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(114, 287);
+            checkBox1.Name = "checkBox1";
+            checkBox1.RightToLeft = RightToLeft.Yes;
+            checkBox1.Size = new Size(35, 19);
+            checkBox1.TabIndex = 21;
+            checkBox1.Text = "SI";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(155, 287);
+            checkBox2.Name = "checkBox2";
+            checkBox2.RightToLeft = RightToLeft.Yes;
+            checkBox2.Size = new Size(44, 19);
+            checkBox2.TabIndex = 22;
+            checkBox2.Text = "NO";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(573, 417);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 23);
+            button1.TabIndex = 23;
+            button1.Text = "CONFIRMAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(676, 417);
+            button2.Name = "button2";
+            button2.Size = new Size(97, 23);
+            button2.TabIndex = 24;
+            button2.Text = "CANCELAR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(11, 415);
+            button3.Name = "button3";
+            button3.Size = new Size(97, 23);
+            button3.TabIndex = 25;
+            button3.Text = "IMPRIMIR";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label22
+            // 
+            label22.AutoSize = true;
+            label22.Location = new Point(11, 325);
+            label22.Name = "label22";
+            label22.Size = new Size(672, 15);
+            label22.TabIndex = 26;
+            label22.Text = "-------------------------------------------------------------------------------------------------------------------------------------";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(70, 30);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(155, 23);
+            textBox1.TabIndex = 27;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(70, 60);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(155, 23);
+            textBox2.TabIndex = 28;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(70, 88);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 29;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(125, 117);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 30;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(286, 30);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(358, 23);
+            textBox5.TabIndex = 31;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(303, 59);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(341, 23);
+            textBox6.TabIndex = 32;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(114, 187);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 34;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(139, 216);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 35;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(479, 187);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 38;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(479, 219);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 39;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(479, 251);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(121, 23);
+            comboBox3.TabIndex = 40;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(114, 248);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(200, 23);
+            comboBox4.TabIndex = 41;
+            // 
+            // textBox7
+            // 
+            textBox7.Enabled = false;
+            textBox7.Location = new Point(120, 361);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(194, 23);
+            textBox7.TabIndex = 42;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox7);
+            Controls.Add(comboBox4);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label22);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(label21);
+            Controls.Add(label20);
+            Controls.Add(label19);
+            Controls.Add(label18);
+            Controls.Add(label17);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+        private Label label19;
+        private Label label20;
+        private Label label21;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label22;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
+        private TextBox textBox7;
+    }
+}
---
+++ Ejercicio AutoRent Express Alan Mogliani 875652/Registro Reserva.cs
@@ -0,0 +1,40 @@
+namespace Ejercicio_AutoRent_Express_Alan_Mogliani_875652
+{
+    public partial class RegistroReserva : Form
+    {
+        public RegistroReserva()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
